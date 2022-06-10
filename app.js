c5f53541a0f229caae786694f3dae1d6c65b06cb
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-06-10 18:36:14 -0400

    updating project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
+const generatePage = require('./dist/index.html');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,10 +129,7 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    
-//     // will be uncommented in lesson 4
-    // const pageHTML = generatePage(mockData);
-    // const pageHTML = generatePage(portfolioData);
+    const pageHTML = generatePage(portfolioData);
 
     fs.writeFile('./index.html', pageHTML, err => {
       if (err) throw new Error(err);
