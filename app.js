0b513eb519d7c040b177fb1a91c9d6bbadda910d
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-06-10 22:19:34 -0400

    finalizing project

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./dist/index.html');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,7 +129,21 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
     fs.writeFile('./dist/index.html', pageHTML, err => {
         if (err) {
@@ -146,4 +160,4 @@ promptUser()
           console.log('Style sheet copied successfully!');
         });
       });
-    })
+    
